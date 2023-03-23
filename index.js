0cb6fa43522a3d755848e6e38d4a85eba74c3d49
Max Dimitrak <maksimdimitrak@Maxs-MacBook-Air.local>
2023-03-23 15:30:14 -0700

    les6

---
+++ index.js
@@ -0,0 +1,40 @@
+var myFirstName = "Maksim";
+var myLastName = "Dimitrak";
+
+///
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";//
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
