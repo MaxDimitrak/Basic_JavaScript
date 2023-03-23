9dcc8216bf7a82adaab3fdaa1389f71471eede01
Max Dimitrak <maksimdimitrak@Maxs-MacBook-Air.local>
2023-03-23 15:46:00 -0700

    next_step

--- index.js
+++ index.js
@@ -37,4 +37,38 @@ console.log(FCC, fact); // Change this line
 const lastName = "Lovelace";
 
 // Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; //
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line// Change this line
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3])
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+let removedFromMyArray = myArray.pop()
+
